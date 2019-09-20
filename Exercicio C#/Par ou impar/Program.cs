005cc811acad89dd0703b19db7a340d6042ede50
Erick Henry <erick_henrry_blv@hotmail.com>
2019-09-20 17:26:35 -0300

    20/19

---
+++ Exercicio C#/Par ou impar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Par_ou_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int num= 1;*/
+            int num = 0;
+            do{                                             /*Digitar 0 ele para */
+            /*whilw(num !=0){ */
+
+                Console.Write("Digite Um Numero: ");      
+            num = int.Parse(Console.ReadLine());            /*Passos necessarios p/ começar */
+
+            if(num % 2 == 0){                               /*if((num % 2) != 0){       ****P/deixar impar primeiro****/ 
+                Console.WriteLine("Numero par");            /*Console.WriteLine("Numero impar"); */
+            } else{
+                Console.WriteLine("Numero impar");
+            }
+
+
+            }while(num !=0);                                /*Digitar 0 ele para */
+
+        }
+    }
+}
