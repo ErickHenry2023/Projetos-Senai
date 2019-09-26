09d61c9c0ba227686d8a4afc5436c726d712459a
Erick Henry <erick_henrry_blv@hotmail.com>
2019-09-26 17:22:13 -0300

    26/09

---
+++ Exercicio C#/Par e Impar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Par_e_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int[] vetor = new int[6];
+            int par = 0;
+            int imp = 0;
+
+
+            for (int cont = 0; cont < 6; cont++){
+                Console.WriteLine($"Digite o {cont+1} número: ");
+                vetor[cont] = int.Parse(Console.ReadLine());
+            }
+
+            foreach(int num in vetor){
+                if (num % 2 == 0){
+                    par++;
+                } else {
+                    imp++;
+                }
+            }
+        
+            Console.WriteLine($"Você tem {par} números pares e {imp} números impares");
+
+        }
+    }
+}
