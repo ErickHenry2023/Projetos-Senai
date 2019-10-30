71319130fd2415bc4405871de9e3577773c71b8b
Erick Henry <erick_henrry_blv@hotmail.com>
2019-10-30 17:33:35 -0300

    30/10

--- Exercicio C#/Desafios de programação/Exercicio 14/Program.cs
+++ Exercicio C#/Desafios de programação/Exercicio 14/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 
-namespace Exercicio_14
+namespace Exercicio14
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         System.Console.WriteLine("Escreva uma verbo: "); 
         string ver = Console.ReadLine();
         
-        if(ver )
+        ver.ToLower();
+
+        if(ver.EndsWith("ar")) 
+        {
+            System.Console.WriteLine("Verbo da 1ª- conjugação");
+
+        } else if (ver.EndsWith("er"))
+        {
+            System.Console.WriteLine("Verbo da 2ª- conjugação");
+        }else if (ver.EndsWith("ir"))
+        {
+            System.Console.WriteLine("Verbo da 3ª- conjugação");
+
+        }   else{
+            System.Console.WriteLine("Verbo nao esta no infinitivo");
+        }
 
         }
     }
