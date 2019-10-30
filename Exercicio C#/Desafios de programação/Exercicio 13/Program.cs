1e1187877d110a10d2d24d7d201ffa0d6d5d411d
Erick Henry <erick_henrry_blv@hotmail.com>
2019-10-30 11:18:58 -0300

    A

---
+++ Exercicio C#/Desafios de programação/Exercicio 13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        
+        System.Console.WriteLine("Digite o 1º valor triângulo: ");
+        num1 = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Digite o 2º valor triângulo: ");
+        num2 = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Digite o 3º valor triângulo: ");
+        num3 = int.Parse(Console.ReadLine());
+
+        if(num1==num2| num1==num3| num2==num3)
+        {
+            System.Console.WriteLine("O triangulo e equilatero");
+        } 
+        else if (num1!=num2| num2==num3| num3!=num1)
+        {
+            System.Console.WriteLine("O triangulo e isoceles");
+        } 
+        else if (num1!=num2| num2!=num3| num3!=num1)
+        {
+            System.Console.WriteLine("O triangulo e escaleno");
+        }
+        else if (num1!=num2 && num2!= num3 && num2 !=num3)
+        {
+            System.Console.WriteLine("E um triangulo qualquer");
+        }
+                
+        }
+    }
+}
---
+++ Exercicio C#/Desafios de programação/Exercicio 14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        System.Console.WriteLine("Escreva uma verbo: "); 
+        string ver = Console.ReadLine();
+        
+        if(ver )
+
+        }
+    }
+}
