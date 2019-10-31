a6772d80e283e186a14a1479a282f4f2d7bc7f00
Erick Henry <erick_henrry_blv@hotmail.com>
2019-10-31 17:16:07 -0300

    31/10

---
+++ Exercicio C#/Desafios de programação/Exercicio 15/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Exercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int n = 0;
+
+            System.Console.WriteLine("Escreva um valor entre 1 a 12"); 
+            //  n = int.Parse(Console.ReadLine());
+            string opcao = Console.ReadLine();
+
+            switch(opcao){
+                case "1":
+                System.Console.WriteLine($" O numero 1 é o mes Janeiro: ");
+                break;
+
+                case "2":
+                System.Console.WriteLine($" O numero 2 é o mes Fevereiro: ");
+                break;
+
+                case "3":
+                System.Console.WriteLine($" O numero 3 é o mes Março: ");
+                break;
+
+                case "4":
+                System.Console.WriteLine($" O numero 4 é o mes Abril: ");
+                break;
+
+                case "5":
+                System.Console.WriteLine($" O numero 5 é o mes Maio: ");
+                break;
+
+                case "6":
+                System.Console.WriteLine($" O numero 6 é o mes Junho: ");
+                break;
+
+                case "7":
+                System.Console.WriteLine($" O numero 7 é o mes Julho: ");
+                break;
+
+                case "8":
+                System.Console.WriteLine($" O numero 8 é o mes Agosto: ");
+                break;
+
+                case "9":
+                System.Console.WriteLine($" O numero 9 é o mes Setembro: ");
+                break;
+
+                case "10":
+                System.Console.WriteLine($" O numero 10 é o mes Outubro: ");
+                break;
+
+                case "11":
+                System.Console.WriteLine($" O numero 11 é o mes Novembro: ");
+                break;
+
+                case "12":
+                System.Console.WriteLine($" O numero 12 é o mes Dezembro: ");
+                break;
+
+                default:
+                    Console.WriteLine("Opção inválida");
+                        break;
+
+            }
+            
+
+
+        }
+    }
+}
---
+++ Exercicio C#/Desafios de programação/Exercicio 16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso = 0;
+            double alt =0;
+
+            System.Console.WriteLine("Digite o seu peso: ");
+            peso = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite a sua altura: ");
+            alt = double.Parse(Console.ReadLine());
+            
+            double imc = peso/ Math.Pow(alt,2);         /* "Math.Pow(alt,2)" é para elevar um numero por 2  */
+            System.Console.WriteLine($"O seu IMC é: {Math.Round(imc, 2).ToString()}");         /*arredondar números decimais "{Math.Round(litros, "2").ToString()}"*/
+
+            if(abaixo de < 20){
+                System.Console.WriteLine($" O seu pesp é abaixo do peso {<imc} ");
+            } else if
+            {
+                
+            }
+
+        }
+    }
+}
