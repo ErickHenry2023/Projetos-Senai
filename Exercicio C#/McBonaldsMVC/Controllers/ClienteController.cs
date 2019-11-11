97ad2c631d895c4fa0661afa49855af26aa2a556
Erick Henry <erick_henrry_blv@hotmail.com>
2019-11-11 18:36:59 -0200

    11/111

--- Exercicio C#/McBonaldsMVC/Controllers/ClienteController.cs
+++ Exercicio C#/McBonaldsMVC/Controllers/ClienteController.cs
@@ -1,12 +1,38 @@
+using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace McBonaldsMVC.Controllers
 {
     public class ClienteController : Controller
     {
+        [HttpGet]
         public IActionResult Login()       /*IActionResult  para mostrar a pag "return View();"*/
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(IFormCollection form)
+        {
+            ViewData["Action"] = "Login";
+            try
+            {
+                System.Console.WriteLine("=================");
+                System.Console.WriteLine(form["email"]);
+                System.Console.WriteLine(form["senha"]);
+                System.Console.WriteLine("=================");
+
+                return View("Sucesso");
+
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.StackTrace);     /*p/  tirar o erro "e" */
+                return View ("Erro");
+            }
+
+
         }
     }
 }
