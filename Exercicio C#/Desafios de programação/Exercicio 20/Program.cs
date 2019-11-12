72ab3de0bff3d61e54e7966fa99934ce8e55949d
Erick Henry <erick_henrry_blv@hotmail.com>
2019-11-12 09:54:03 -0300

    notbook

--- Exercicio C#/Desafios de programação/Exercicio 20/Program.cs
+++ Exercicio C#/Desafios de programação/Exercicio 20/Program.cs
@@ -6,8 +6,16 @@ namespace Exercicio20
     {
         static void Main(string[] args)
         {
-            Random aleatorio = new Random();
-            System.Console.WriteLine(aleatorio.Next(1000));
+            Console.Clear();
+            int [] vetor = new int [10];
+            Random Random  = new Random();
+            for(int i = 0;i<vetor.Length;i++) vetor[i] = Random.Next(1000);
+            for(int i = 0; i<vetor.Length;i++) if (vetor[i]%2==0) System.Console.WriteLine($"{vetor[i]} Par");
+            System.Console.WriteLine();
+            for(int i = 0; i<vetor.Length;i++) if (vetor[i]%2==1) System.Console.WriteLine($"{vetor[i]} impar");
+
+
+            // System.Console.WriteLine(aleatorio.Next(1000));
 
         }
     }
